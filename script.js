07c04528afbe24a1184f7772abd36d5d57d16625
purnima11apr <141928600+purnima11apr@users.noreply.github.com>
2023-09-08 19:06:08 +0530

    created a submission

--- script.js
+++ script.js
@@ -3,3 +3,31 @@ function firstNonRepeatedChar(str) {
 }
 const input = prompt("Enter a string");
 alert(firstNonRepeatedChar(input)); 
+
+function firstNonRepeatedChar(str) {
+  if (str.length === 0) {
+    return null;
+  }
+
+	const charCount = {}; 
+
+	for (let i = 0; i < str.length; i++) {
+    const char = str[i];
+    if (charCount[char]) {
+      charCount[char]++;
+    } else {
+      charCount[char] = 1;
+    }
+  }
+
+	 for (let i = 0; i < str.length; i++) {
+    const char = str[i];
+    if (charCount[char] === 1) {
+      return char; 
+    }
+  }
+
+	 return null;
+
+}
+
